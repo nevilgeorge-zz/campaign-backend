811a519ee4fb017b93dacf8126134d84750db9d5
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-14 14:26:22 -0500

    initial express server commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+
+var express = require('express'),
+	app = express();
+
+app.set('port', 8000);
+
+app.get('/', function(req, res) {
+	res.send('Hi Nevil');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Listening on port ' + app.get('port') + '...');
+});
