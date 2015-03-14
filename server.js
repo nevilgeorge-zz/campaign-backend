aad0627db0f5c4ad16b715d183e5bd44e7357357
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-14 14:44:53 -0500

    adding next route

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var express = require('express'),
 app.set('port', (process.env.PORT || 8000));
 
 app.get('/', function(req, res) {
-	res.send('Hi Nevil');
+	res.send('You\'ve reached a Node server.');
+});
+
+app.get('feedback', function(req, res) {
+	res.send('feedback');
 });
 
 app.listen(app.get('port'), function() {
