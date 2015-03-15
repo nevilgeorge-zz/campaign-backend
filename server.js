d2f398759fcd102c9172d803db379c41fdeec746
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-15 14:47:20 -0500

    adding instagram capabilities and deploying

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 var express = require('express'),
 	bodyParser = require('body-parser'),
 	nodemailer = require('nodemailer'),
+	insta = require('instagram-node').instagram(),
+	request = require('request'),
 	app = express();
 
 // nodemailer transporter
@@ -13,6 +15,12 @@ var transporter = nodemailer.createTransport({
 		user: 'listenthenlead@gmail.com',
 		pass: process.env.EMAIL_PASS
 	}
+});
+
+// initialize instagram API
+insta.use({
+	client_id: process.env.INSTA_CLIENT_ID,
+	client_secret: process.env.INSTA_CLIENT_SECRET
 });
 
 // setting up express app
@@ -44,7 +52,50 @@ app.get('/feedback', function(req, res) {
 	});
 });
 
+// var redirect_uri = 'http://localhost:8000/handleauth';
 
+// app.get('/authorize_user', function(req, res) {
+// 	res.redirect(insta.get_authorization_url(redirect_uri, {scope: ['likes']}))
+// });
+
+// app.get('/handleauth', function(req, res) {
+// 	insta.authorize_user(req.query.code, redirect_uri, function(err, result) {
+// 		if (err) {
+// 			console.log(err);
+// 			res.send('authrorization failed');
+// 		} else {
+// 			console.log(result.access_token);
+// 			res.send(result.access_token);
+// 		}
+// 	});
+// });
+
+// get embedded html from instagram, PROVIDED YOU HAVE AN ACCESS TOKEN
+app.get('/instagram', function(req, res) {
+	var access_token = process.env.INSTA_ACCESS_TOKEN;
+	request.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=' + access_token, function(err, response, body) {
+		if (err) {
+			console.log(err);
+			res.send(err);
+			return;
+		}
+		var items = JSON.parse(body).data,
+			embeddedLinks = [],
+			count = 0,
+			link;
+		for (var i = 0; i < items.length; i++) {
+			link = items[i].link;
+			request.get('http://api.instagram.com/oembed?url=' + link, function(err, resp, body) {
+				obj = JSON.parse(body);
+				embeddedLinks.push(obj.html);
+				count++;
+				if (count == items.length) {
+					res.send(embeddedLinks);
+				}
+			});
+		}
+	});
+});
 
 app.listen(app.get('port'), function() {
 	console.log('Listening on port ' + app.get('port') + '...');
