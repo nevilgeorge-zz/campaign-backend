dd8b4c64d2573537dfb1cf950f335dbed8f65345
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-14 19:12:46 -0500

    adding mail capabilities

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 // server.js
 
 var express = require('express'),
+	bodyParser = require('body-parser'),
+	nodemailer = require('nodemailer'),
+	mail = nodemailer.mail,
 	app = express();
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'liven93@gmail.com',
+		pass: 'george93'
+	}
+});
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('port', (process.env.PORT || 8000));
 
@@ -10,7 +24,22 @@ app.get('/', function(req, res) {
 });
 
 app.get('/feedback', function(req, res) {
-	res.send('feedback');
+	console.log(req.query);
+	transporter.sendMail({
+		from: 'Noah + Christina ' + req.query.from,
+		to: 'nevil@u.northwestern.edu',
+		subject: 'Email from We\'re Listening form',
+		replyTo: req.query.from,
+		text: req.query.text
+	}, function(error, info) {
+		if (error) {
+			console.log(error);
+			res.send(error);
+		} else {
+			console.log(info);
+			res.send(info.response);
+		}
+	});
 });
 
 app.listen(app.get('port'), function() {
