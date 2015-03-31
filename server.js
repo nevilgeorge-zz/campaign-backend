1c26d60bf80a073836a57f5a3091b20e0ce96214
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-03-30 22:24:05 -0500

    oops typo

--- server.js
+++ server.js
@@ -106,10 +106,10 @@ app.get('/instagram', function(req, res) {
 });
 
 var twitter = new Twitter({
-	consumer_key: process.env.TWITTER_CONSUMER_KEY
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
 	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token_key: process.env.ACCESS_TOKEN_KEY,
-	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 app.get('/twitter', function(req, res) {
